a9ea701ccf52ccc9dacfe642f7bbd93e477e095c
vivy93 <vivikadar93@gmail.com>
2017-11-23 23:43:03 +0100

    Lobby
    Now it works with one button.

---
+++ dratharg_I/Assets/Scripts/HostGame.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+
+public class HostGame : MonoBehaviour {
+
+	[SerializeField]
+	private uint roomSize = 4;
+	private string roomName;
+	private NetworkManager _networkManager;
+	
+	// Use this for initialization
+	void Start () {
+		_networkManager = NetworkManager.singleton;
+		if (_networkManager.matchMaker ==null)
+		{
+			_networkManager.StartMatchMaker();
+		}
+	}
+
+	public void SetRoomName(string _name)
+	{
+		roomName = _name;
+
+	}
+	
+	public void CreateRoom()
+	{
+		if(roomName != "" && roomName != null)
+		{
+			Debug.Log("Creating Room: " + roomName + " with room for " + roomSize + " players.");
+			//Create room
+			_networkManager.matchMaker.CreateMatch(roomName, roomSize, true, "", "", "", 0, 0, _networkManager.OnMatchCreate);
+		}
+		
+	}
+}
---
+++ dratharg_I/Assets/Scripts/RoomListItem.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Networking.Match;
+
+public class RoomListItem : MonoBehaviour {
+	public delegate void JoinRoomDelegate(MatchInfoSnapshot _match);
+
+	public JoinRoomDelegate joinRoomCallback;
+
+	[SerializeField]
+	private Button roomButton;
+
+	[SerializeField]
+	private Text roomNameText;
+
+	private MatchInfoSnapshot match;
+
+	// Use this for initialization
+	public void Setup (MatchInfoSnapshot _match,  JoinRoomDelegate _joinRoomCallback) {
+		
+		match = _match;
+		joinRoomCallback =_joinRoomCallback;
+		//roomNameText.text = match.name + " (" + match.currentSize + " )";
+		roomButton.GetComponentInChildren<Text>().text = match.name + " (" + match.currentSize + " )";
+		Debug.Log ("RoomListItem////Setup");
+	}
+	
+	public void JoinGameMethod () {
+		Debug.Log ("JoinRoom");
+		joinRoomCallback.Invoke(match);
+	}
+	public void Test () {
+		roomButton.enabled = true;
+		this.GetComponentInChildren<Text>().text = "CSŐŐ";
+	}
+
+}
