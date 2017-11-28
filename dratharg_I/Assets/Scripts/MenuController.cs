5ef4b4c0cad33d396d2a2b872014085a9e76a91f
vivy93 <vivikadar93@gmail.com>
2017-11-28 21:46:06 +0100

    Menu
    Login, Register, Menu finished. Need: scoreboard, map design

---
+++ dratharg_I/Assets/Scripts/MenuController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuController : MonoBehaviour {
+	int count = 0;
+
+	///////////////////////////////////////////////////
+	//				Button functions				 //
+	///////////////////////////////////////////////////
+
+	public void LoadScene(string scenesName)
+	{
+		SceneManager.LoadScene(scenesName);
+	}
+
+	public void ExitGame()
+	{
+		Application.Quit();
+	}
+
+	public void TurnOffOnMusic()
+	{
+		count += 1;
+		if (count % 2 == 1) {			
+			AudioListener.pause = true;
+		} else {
+			AudioListener.pause = false;
+		}
+
+	}
+
+}
